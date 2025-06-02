991fee5fc559e9fff24b4d2348db14dac2eb84d8
Roman Myachin <roma.mellon@mail.ru>
2025-06-02 10:11:17 +0500

    add code

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_list, product_detail
 from django.contrib import admin
+
 
 app_name = CatalogConfig.name
 
@@ -9,4 +10,6 @@ urlpatterns = [
     path("", home, name="home"),
     path("", contacts, name="contacts"),
     path("admin/", admin.site.urls),
+    path('', product_list, name='product_list'),
+    path('products/<int:pk>/', product_detail, name='product_detail')
 ]
