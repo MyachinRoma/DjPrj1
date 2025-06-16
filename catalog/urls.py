3b0b41ea4633976009f584f2179bf54b33d65d50
Roman Myachin <roma.mellon@mail.ru>
2025-06-16 11:52:45 +0500

    add README

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('catalog/create', ProductCreateView.as_view(), name='product_create'),
     path('catalog/<int:pk>/update', ProductUpdateView.as_view(), name='product_update'),
-path('catalog/<int:pk>/delete', ProductDeleteView.as_view(), name='product_delete')
+    path('catalog/<int:pk>/delete', ProductDeleteView.as_view(), name='product_delete')
 ]
